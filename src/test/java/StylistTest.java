33d31eb117555219c442a1af1cc5ff76f626a8a8
Eric Krause <krause197@gmail.com>
2016-09-23 13:11:26 -0700

    first two tests for stylistTest.java instantiates and get id.  Tests successful

--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -1,8 +1,25 @@
 import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class StylistTest {
+  private Stylist stylist;
+
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
+    stylist = new Stylist("Mahesh Ankur", "Mahesh produces a great fade and high and tight hair styles");
+  }
+
+  @Test
+  public void Stylist_instantiates_true() {
+    assertTrue(stylist instanceof Stylist);
+  }
+
+  @Test
+  public void Stylist_getId_true() {
+    assertTrue(stylist.getId()) > 0);
+  }
+
+  
